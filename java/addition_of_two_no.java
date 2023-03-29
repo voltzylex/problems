909c77a686df3626e37f7c1a29c7ddf57d7e78bd
Sushil <baymax732001@gmail.com>
2023-03-29 22:18:57 +0530

    additon of two number

---
+++ java/addition_of_two_no.java
@@ -0,0 +1,9 @@
+//  Wednesday March 29,10:18:05 PM
+// https://practice.geeksforgeeks.org/problems/addition-of-two-numbers0812/1?page=1&difficulty[]=-2&status[]=unsolved&sortBy=submissions
+class Solution{
+    static int addition(int A, int B){
+        // code here
+        // System.out.print(A+B);
+        return A+B;
+    }
+}
