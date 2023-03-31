9982d6226906f0e147eb5eed8d70350be1c06448
Sushil <baymax732001@gmail.com>
2023-03-31 22:00:09 +0530

    convert to lower case

---
+++ java/to_lower_case.java
@@ -0,0 +1,8 @@
+//  Friday March 31, 9:59:00 PM 
+// https://practice.geeksforgeeks.org/problems/java-convert-string-to-lowercase2313/1?page=1&difficulty[]=-2&status[]=unsolved&sortBy=submissions
+class Solution {
+    static String toLower(String S) {
+        // code here
+        return S.toLowerCase();
+    }
+}
