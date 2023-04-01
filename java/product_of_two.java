5e4d3575b4b8ac5f143f9bd1fba87516d1a90cb7
Sushil <baymax732001@gmail.com>
2023-04-01 22:53:04 +0530

    how to find product of two value's

---
+++ java/product_of_two.java
@@ -0,0 +1,9 @@
+// https://practice.geeksforgeeks.org/problems/c-input-output2432/1?page=1&difficulty[]=-2&status[]=unsolved&sortBy=submissions
+//  Saturday April 1,10:51:53 Pm
+//User function Template for Java
+class Solution{
+    static int multiplication(int A, int B){
+        // code here
+        return A*B;
+    }
+}
