0024ce1ef0d1551e0e06daa9398f9eb3c4c6030c
Sushil <baymax732001@gmail.com>
2023-04-02 22:38:05 +0530

    odd or even

---
+++ java/odd_even.dart
@@ -0,0 +1,14 @@
+// Sunday April 2, 10:37:03 PM
+//  https://practice.geeksforgeeks.org/problems/odd-or-even3618/1?page=2&difficulty[]=-2&status[]=unsolved&sortBy=submissions
+//User function Template for Java
+class Solution{
+    static String oddEven(int N){
+        // code here
+        if(N%2==0){
+            return "even";
+        }
+        else{
+            return "odd";
+        }
+    }
+}
