573ef1b029af3e39255ca72859fc221f25c97252
Sushil <baymax732001@gmail.com>
2023-04-03 20:43:26 +0530

    sum of series

---
+++ java/sum_of_series.java
@@ -0,0 +1,12 @@
+// Moday April 3, 8:40:27
+// https://practice.geeksforgeeks.org/problems/sum-of-series2811/1?page=1&difficulty[]=-2&status[]=unsolved&sortBy=submissions
+class Solution {
+    // function to return sum of  1, 2, ... n
+    
+    long seriesSum(int n) {
+        // code here
+        long sum=n;
+        
+        return sum*(sum+1)/2;
+    }
+}
